616c747ee767a0c35fa0f768d65fd6487da509f5
Matthew Denune <mattdenune@gmail.com>
2020-10-01 11:00:19 -0400

    merged conflicts

---
+++ src/app.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlbars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+
+// Routes
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather App",
+        name: "Matt Denune"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: "Matt Denune"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        name: 'Matt Denune',
+        helpText: 'This is some helpful text'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Matt Denune',
+        error: 'Help article not found'
+    })
+})
+
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Sunny',
+        location: 'Brooklyn'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Matt Denune',
+        error: 'Page not found'
+    })
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
