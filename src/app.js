e961005b8b5e66be2ce5dc8190ce3d7d3247572a
Matthew Denune <mattdenune@gmail.com>
2020-10-01 11:28:08 -0400

    brought weather APIs over from another project

--- src/app.js
+++ src/app.js
@@ -51,9 +51,27 @@ app.get('/help/*', (req, res) => {
 
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
     res.send({
         forecast: 'Sunny',
-        location: 'Brooklyn'
+        location: 'Brooklyn',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: "You must provide a search term"
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
     })
 })
 
---
+++ src/utlis/forecast.js
@@ -0,0 +1,17 @@
+const request = require('postman-request');
+
+
+const forecast = (longitude, latitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=bd3261008503a17797e8570d8dfa32f7&query=${latitude},${longitude}&units=f`
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather services', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            console.log(`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+        }
+    })
+}
+
+module.exports = forecast;
